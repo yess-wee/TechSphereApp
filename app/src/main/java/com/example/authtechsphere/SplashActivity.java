8e99d059a88a33128c22413940e2553fba5f58e6
yess-wee <yashvichauhan83@gmail.com>
2024-03-30 00:20:38 +0530

    splash screen added

---
+++ app/src/main/java/com/example/authtechsphere/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.example.authtechsphere;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    TextView appname;
+    LottieAnimationView lottie;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        appname = findViewById(R.id.appname);
+        lottie = findViewById(R.id.lottie);
+
+        appname.animate().translationY(-1400).setDuration(2700).setStartDelay(0);
+        lottie.animate().translationX(2000).setDuration(2000).setStartDelay(2900);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(i);
+            }
+        },5000);
+    }
+}
